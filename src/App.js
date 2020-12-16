144ad22db85cdb80b092d979a494f386b11531fe
academicodan <65352299+academicodan@users.noreply.github.com>
2020-12-16 10:21:22 -0300

    implementation to add a task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 
 export default function App() {
+  const[tasks, setTask] = useState([])
+  const addStack = task => {
+    setTask([...tasks, task ])
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Form onSave={addStack}/>
+      <List tasks={tasks}/>
+    </div>
+  );
+}
+
+export function List(props){
+  const{ tasks } = props;
+  return(
+    <div>
+      {tasks.map(task => {
+        return(
+          <div>
+            <span>{task}</span>
+            <button>Delete</button>
+            <button>Check</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export function Form(props){
+  const{ onSave } = props;
+  const[text, setText] = useState("");
+  return(
+    <div>
+      <input onChange={e => setText(e.currentTarget.value)}/>
+      <button onClick={()=> onSave(text)}>Add</button>
     </div>
   );
 }
